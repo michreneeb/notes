1a69f12faefab027301d2bafd43ad873b4866414
michreneeb <michelle.black2@georgebrown.ca>
2024-11-22 15:50:17 -0500

    Update lab12.sql

--- lab12.sql
+++ lab12.sql
@@ -1,1 +1,24 @@
+--SQL Queries Part 2
 
+--Concatenate Course Name and Semester:
+
+--Find Courses with Labs on Fridays:
+select course_id, course_name, lab_time
+FROM courses
+where lab_time LIKE 'FRI%';
+
+--Upcoming Assignments:
+SELECT *
+FROM  assignments
+WHERE due_date > '2024-11-22'
+ORDER BY due_date;
+
+--Count Assignments by Status:
+
+--Longest Course Name:
+
+--Uppercase Course Names:
+
+--Assignments Due in September:
+
+--Assignments with Missing Due Dates:
