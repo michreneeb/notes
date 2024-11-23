a1e2936ac91f1839e3c04c2a97e2deea12f3428f
michreneeb <michelle.black2@georgebrown.ca>
2024-11-23 13:19:43 -0500

    Commented out lines lab11.sql

--- lab11.sql
+++ lab11.sql
@@ -1,37 +1,35 @@
-Query 1
+--Query 1
 SELECT title, due_date
 FROM assignments
 where course_id = 'COMP1234'
 order by due_date;
 
-Query 2
+--Query 2
 select title, min (due_date)
 from assignments;
 
-Query 3
+--Query 3
 SELECT title, max(due_date)
 from assignments;
 
-Query 4
+--Query 4
 SELECT course_id, title
 from assignments
 where due_date = '2024-10-08';
 
-Query 5
+--Query 5
 select title, due_date
 from assignments
 where due_date LIKE '2024-10%';
 
-Query 6
+--Query 6
 SELECT title, due_date
 from assignments
 where status = 'Completed'
 order by due_date;
 
-Optional Tasks
-
+--Optional Tasks
 Count of Not Started Assignments
-
 select count(*)
 from assignments
 where status = 'Not Started';
