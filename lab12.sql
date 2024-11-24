1c6cb567a67b9725899d8cc494420d4781acddba
michreneeb <michelle.black2@georgebrown.ca>
2024-11-24 14:40:19 -0500

    Finished lab12.sql

--- lab12.sql
+++ lab12.sql
@@ -1,28 +1,36 @@
 --SQL Queries Part 2
 
---Concatenate Course Name and Semester:
+--1. Concatenate Course Name and Semester
+select course_name || '-' || semester
+from courses;
 
---Find Courses with Labs on Fridays:
+--2. Find Courses with Labs on Fridays
 select course_id, course_name, lab_time
 FROM courses
 where lab_time LIKE 'FRI%';
 
---Upcoming Assignments:
+--3. Upcoming Assignments
 SELECT *
 FROM  assignments
 WHERE due_date > '2024-11-22'
 ORDER BY due_date;
 
---Count Assignments by Status:
+--4. Count Assignments by Status
+select status, COUNT(*) from assignments
+group by status;
 
---Longest Course Name:
+--5. Longest Course Name
+select course_name, length(course_name) from courses
+order by length(course_name) DESC;
 
---Uppercase Course Names:
+--6. Uppercase Course Names
 select upper(course_name) from courses;
 
---Assignments Due in September:
+--7. Assignments Due in September
 SELECT * FROM assignments
 where due_date Like '2024-09%'
 order by due_date;
 
---Assignments with Missing Due Dates:
+--8. Assignments with Missing Due Dates
+select * from assignments
+where due_date ISNULL;
